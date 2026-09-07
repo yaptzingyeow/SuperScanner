ce9744279a1ffef7a79c30d4ccbde53833735733
Salesworks-TzingYeow <yaptzingyeow@gmail.com>
2026-09-07 16:23:08 +0800

    config: connect Firebase web app

--- apps/web/src/environments/environment.ts
+++ apps/web/src/environments/environment.ts
@@ -3,14 +3,14 @@ export const environment = {
   e2e: false,
   apiBaseUrl: '/api',
   firebase: {
-    apiKey: '',
-    authDomain: '',
-    projectId: '',
-    storageBucket: '',
-    messagingSenderId: '',
-    appId: '',
+    apiKey: 'AIzaSyBnXleQbbtwGrgYL4_ww6yH1mgWp-1Atgg',
+    authDomain: 'superscanner-dev.firebaseapp.com',
+    projectId: 'superscanner-dev',
+    storageBucket: 'superscanner-dev.firebasestorage.app',
+    messagingSenderId: '122650027029',
+    appId: '1:122650027029:web:074aeaa1f2ac7a3d471a1',
+    measurementId: 'G-6C76D2NVHF'
   },
-  appCheckEnterpriseSiteKey: '',
+  appCheckEnterpriseSiteKey: '6Lffaq0tAAAAAH5EHzifubg3Qe0d7erI1DoBP3lx',
 } as const;
-
 
